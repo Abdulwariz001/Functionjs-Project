428799712fe78b2a74fa10b43ebd7a0bf205f1a3
Azeez Wariz <warizazeez05@gmail.com>
2022-11-22 08:18:21 +0100

    function project

---
+++ script.js
@@ -0,0 +1,41 @@
+    
+function sub(){
+        let d =100;
+        let e =5;
+        let f = d-e
+        return f
+     }
+     console.log(sub())
+
+   
+function multi(){
+        let k =10;
+        let l =5;
+        let m = k*l
+        return m
+     }
+     console.log(multi())
+   
+function div(){
+        let r =10;
+        let s =5;
+        let t = r/s
+        return t
+     }
+     console.log(div())
+     
+function mod(){
+        let u =10;
+        let v =5;
+        let w = u%v
+        return w
+     }
+     console.log(mod())
+    
+function expo(){
+        let x =10;
+        let y =5;
+        let z = x**y
+        return z
+     }
+     console.log(expo())
